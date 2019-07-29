cfaece541bee5431091d377212cf9ecfe7c789d8
ryotauetani1221 <r.uetani33@gmail.com>
2019-07-29 19:58:11 +0900

    main.jsで読み込んでいたfirebase周りをプラグイン化

--- src/main.js
+++ src/main.js
@@ -2,18 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import firebase from 'firebase'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB7vjVp-_PxmWDDqtg1R_wSy27031iUehs",
-  authDomain: "vue-nagezeni.firebaseapp.com",
-  databaseURL: "https://vue-nagezeni.firebaseio.com",
-  projectId: "vue-nagezeni",
-  storageBucket: "",
-  messagingSenderId: "764937800237",
-  appId: "1:764937800237:web:a0be24a8f5cdefa5"
-};
-firebase.initializeApp(firebaseConfig);
+import './plugin/firebase'
 
 Vue.config.productionTip = false
 
---
+++ src/plugin/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB7vjVp-_PxmWDDqtg1R_wSy27031iUehs",
+    authDomain: "vue-nagezeni.firebaseapp.com",
+    databaseURL: "https://vue-nagezeni.firebaseio.com",
+    projectId: "vue-nagezeni",
+    storageBucket: "",
+    messagingSenderId: "764937800237",
+    appId: "1:764937800237:web:a0be24a8f5cdefa5"
+};
+firebase.initializeApp(firebaseConfig);
